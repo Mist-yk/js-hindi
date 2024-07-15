76bacc6ffadbec928daf13cc3a85d343583c5deb
Mist-yk <ajnegi456@gmail.com>
2024-07-15 10:25:11 +0000

    variable done config

---
+++ 01_basics/variable.js
@@ -0,0 +1,14 @@
+const accountid =144553
+let accountEmail ="ajnum@gmail.com"
+var accountpass="12345"
+accounCity="uk"
+
+//accountid=2//not allowed
+
+accountEmail="jabdk"
+accountpass="3242"
+accountCity="ku"
+
+console.log(accountid);
+
+console.table([accountid,accountEmail,accountpass,accountCity])
