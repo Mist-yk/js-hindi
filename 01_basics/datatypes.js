321a3e8029b535e1a671f4ec8c5a8dc34cb1a573
Mist-yk <ajnegi456@gmail.com>
2024-07-16 06:31:15 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict";//treat all JS code as newer version
+
+//alert("hello")//we are using node js not browser
+
+let name = "Ajay"
+let age = 19
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null =>standlone value
+//undefined =>value not assigned
+//symbol => unique
+
+
+//object
+
+console.log(typeof"ajay");
