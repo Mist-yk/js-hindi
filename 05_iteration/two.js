d1aa86bdcc33bf81c261b450590685a9f3465a8a
Mist-yk <ajnegi456@gmail.com>
2024-07-25 16:03:17 +0000

    do while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,20 @@
+// let i = 0
+// while (i<= 10) {
+//     console.log(`value of index ${i}`);
+//     i = i+2
+// }
+
+let myArray = ["atrain","deep","butcher"]
+
+let arr =0
+while(arr<myArray.length){
+    console.log(`value is ${myArray[arr]}`);
+    arr =arr +1
+}
+
+let her =1
+
+do {
+    console.log(`her count is ${her}`);
+    her++
+} while (her <=2);
