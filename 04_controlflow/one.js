9e2f8b140298aa37f61d86da091000080294f041
Mist-yk <ajnegi456@gmail.com>
2024-07-25 05:10:42 +0000

    ifelse

---
+++ 04_controlflow/one.js
@@ -0,0 +1,41 @@
+// if
+
+// if (true){
+//     //body
+// }
+// else{
+//     //body
+// }
+
+// const score = 200
+
+// if (score > 100){
+//     let power = "fly"
+//     console.log(`user power ${power}`);
+// }
+// console.log(`user power ${power}`);
+
+
+// const balance = 1000
+// if(balance > 500) console.log("test");
+
+//Else if
+// if (sd) {
+
+// }else if(){
+
+// }else {
+
+// }
+
+const userLoggedIn = true
+const debitcard =  true
+const loggenInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitcard){
+    console.log("you can buy nigga");
+}
+if(loggenInFromGoogle || loggedInFromEmail  ){
+    console.log("user logged in");
+}
