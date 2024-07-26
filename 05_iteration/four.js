5fcac8878a7446a864d3cff665ba3872a5eaa51c
Mist-yk <ajnegi456@gmail.com>
2024-07-26 08:02:06 +0000

    for in

---
+++ 05_iteration/four.js
@@ -0,0 +1,17 @@
+// for in
+const myobj = {
+    js :'javascript',
+    cpp:'c++',
+    py:'python'
+}
+
+for (const key in myobj) {
+   //console.log(`${key} shortcut is for ${myobj[key]}`);
+}
+
+
+const programming = ["js","py","cpp"]
+
+for (const key in programming) {
+   //console.log(programming[key]);
+}
