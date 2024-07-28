7fbf55fe8ec49f48649f7199283d88fa2f89cfc6
Mist-yk <ajnegi456@gmail.com>
2024-07-28 08:39:34 +0000

    map

--- 05_iteration/six.js
+++ 05_iteration/six.js
@@ -8,10 +8,30 @@
 // console.log(val);
 
 // Filter (it return values)
-const mynums = [1,2,3,4,5,6,7,8]
+//const mynums = [1,2,3,4,5,6,7,8]
 
 // const newnum = mynums.filter( (num) => num > 4 )
-const newnum = mynums.filter( (num) => {
-    return num >4
-} )
-console.log(newnum);
+// const newnum = mynums.filter( (num) => {
+//     return num >4
+// } )
+
+// const newnum = []
+// mynums.forEach( (num) =>{
+//     if(num > 4){
+//         newnum.push(num)
+//     }
+    
+// })
+// console.log(newnum);
+
+
+//MAP
+const mynum = [1,2,3,4,5,6,7,8]
+
+//const newnums = mynum.map( (num) => num + 10)
+
+const newnums = mynum
+        .map( (num) => num * 10)
+        .map((num) => num + 1)
+        .filter( (num) => num >= 40)
+console.log(newnums)
