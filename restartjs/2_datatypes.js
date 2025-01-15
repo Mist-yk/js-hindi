12bc769015371472ffdf248dd25d8439d97e78ea
Ajay Negi <ajnegi456@gmail.com>
2025-01-15 02:55:47 +0000

    data types n probs

--- restartjs/2_datatypes.js
+++ restartjs/2_datatypes.js
@@ -27,3 +27,37 @@
 //   "Rohan": undefined
 // }
 // console.log(items["Ajay"]);
+
+
+
+
+
+
+
+//Problems
+// //creating a string and adding a number to it
+// let a="ajay";
+// let b=5;
+// let c = a+b;
+// console.log(c);
+
+// console.log(typeof c );
+
+// //create const object and try to change it to hold a number later
+// const a1={
+//   name:"ajay",
+//   section:1,
+//   isPrincipal:false
+// }
+// //a1 =45;
+// a1['friend'] = "shubham";
+// a1['name'] = "luv";
+// console.log(a1);
+
+// //creat const dictionary
+// const dict ={
+//   appreciate:"recognize the full worth of",
+//   ataraxia:"a state of freedom from emotional disturbance and anxiety",
+//   yakka:"work,especially hard work"
+// }
+// console.log(dict);
