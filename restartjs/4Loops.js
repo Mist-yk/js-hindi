f695ced0a568f59eb8209e7a3604276c801bb70f
Ajay Negi <ajnegi456@gmail.com>
2025-01-15 18:52:30 +0000

    Loops

---
+++ restartjs/4Loops.js
@@ -0,0 +1,40 @@
+// //For loop
+// //program to add first n natural numbers
+// let sum =0;
+// let n=prompt("enter a number");
+// n=Number.parseInt(n);
+// for(let i=1;i<10;i++){
+//   sum+=i;
+  
+// }
+// console.log(sum);
+
+
+// //program to print factorial
+// let fact =1 ;
+// let n=prompt("enter a number");
+// n=Number.parseInt(n);
+// for(let i=1;i<10;i++){
+//   fact =(fact*i);
+  
+// }
+// console.log(fact);
+
+
+
+// //For in loop
+
+// let obj ={
+//   ajay :200,
+//   anshu :300,
+//   shivansh : 500
+// }
+
+// for(let a in obj){
+//   console.log(a);
+// }
+
+// //For of loop
+// for(let b of "ajay"){
+//   console.log(b);
+// }
