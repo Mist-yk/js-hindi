0db48600cecb9e606547279530f8d794adc890cc
Ajay Negi <ajnegi456@gmail.com>
2025-01-15 02:38:57 +0000

    data types

---
+++ restartjs/2_datatypes.js
@@ -0,0 +1,29 @@
+// console.log("Primptive Data Types and non primptive");
+
+// //Premptive data types
+// // n-null
+// // n-number
+// // s-string
+// // s-symbol
+// // b-boolean
+// // b-bigint
+// // u-undefined
+
+// let a=null;
+// let b= 345;
+// let c= true;
+// let d = BigInt("567") + BigInt("3");
+// let e = "Harry";
+// let f = Symbol('I am a nice symbol');
+// let g = undefined;
+// console.log(a,b,c,d,e,f,g);
+// console.log(typeof c);
+
+// //Objects or Non-primptive
+// const items ={
+//   "Ajay": true,
+//   "Shubh": false,
+//   "Lovish": 67,
+//   "Rohan": undefined
+// }
+// console.log(items["Ajay"]);
