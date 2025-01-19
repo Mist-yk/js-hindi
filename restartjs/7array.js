0aa55f5b52663a5d3f9d376a911c5b6dbb77f507
Ajay Negi <ajnegi456@gmail.com>
2025-01-19 09:14:04 +0000

    Arrays

---
+++ restartjs/7array.js
@@ -0,0 +1,40 @@
+// let arr =[1,2,3,4];
+// console.log(arr);
+
+// //printing array using for loop
+// let ar =[1,2,3,4,5];
+// for(let i=0;i<ar.length;i++){
+//   console.log(ar[i]);
+// }
+
+//To string and join method
+// let num =[1,2,4,5];
+// let b = num.toString();
+// console.log(b);
+// let c =[,2,3,45];
+// let d =num.join("_");
+// console.log(d);
+
+// let num =[1,2,4,5];
+// num.push(6)
+// console.log(num)
+
+
+// //for each loop 
+// let num =[1,2,4,5];
+
+// num.forEach((element) => {
+//   console.log(element*element);
+// })
+
+//Array from(to convert obj in array)
+
+// let name="ajay";
+// let arr = Array.from(name);
+// console.log(arr);
+
+//for of in array
+// let nam =[1,2,3,4,5];
+// for(let i of nam){
+//     console.log(i);
+// }
