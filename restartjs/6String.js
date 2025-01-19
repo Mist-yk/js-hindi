ae24dec38aaebe9aeef126147a7f562b10a4ce0d
Ajay Negi <ajnegi456@gmail.com>
2025-01-19 06:27:35 +0000

    strings

---
+++ restartjs/6String.js
@@ -0,0 +1,21 @@
+let name="ajay";
+console.log(name.length);
+
+//use of templet litral
+let boy1="ajay";
+let boy2="anshu2";
+
+let c = `${boy2} is a friend of ${boy1}`
+console.log(c);
+
+//Escape Sequence character
+// let fruit = 'bana\'na';
+// console.log(fruit); //output = bana'na
+
+let fruit = 'bana\na';
+console.log(fruit); //output = banan (a will b3 printed in nxt line)
+
+
+// /r
+let fruit2 = 'bana\rn';
+console.log(fruit2);
