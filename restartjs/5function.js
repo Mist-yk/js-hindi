be01631fbdd28b77c00bffe665aecf78a33836f1
Ajay Negi <ajnegi456@gmail.com>
2025-01-19 06:06:30 +0000

    functions

---
+++ restartjs/5function.js
@@ -0,0 +1,45 @@
+// //Functions in js
+//  function avg(x,y){
+//    return (x+y)/2;
+//  }
+
+// //Arrow Function
+
+// const sum =(p,q)=>{
+//   return p + q;
+// } 
+
+// const hello = ()=>{
+//   console.log("hey how u doin");
+// return "hi";
+// }
+
+// let v =hello();
+
+// //print marks of students in in an object
+// const marks ={
+//   ajay :98,
+//   anshu:24,
+//   aman:45
+// }
+// for(let key in marks){
+//   console.log("marks =" +key+"are"+marks[key])
+  
+// }
+
+// //user enter a number if it is smae as our number good wrna try again
+// let n=12;
+// let un;
+// while(un!=n){
+//   console.log("try again");
+//   un = prompt("dubara daal");
+  
+// }
+// console.log("sbaas");
+
+
+// //to print mean of five numbers
+// const mean = (a ,b , c ,d ,e)=>{
+//   return (a+b+c+d+e)/5;
+// }
+// console.log(mean(1,2,3,4,5));
