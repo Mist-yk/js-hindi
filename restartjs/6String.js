bbc08ed36de798ad9d4198c264167b97679a508c
Ajay Negi <ajnegi456@gmail.com>
2025-01-19 07:38:21 +0000

    string_methods

--- restartjs/6String.js
+++ restartjs/6String.js
@@ -1,21 +1,48 @@
-let name="ajay";
-console.log(name.length);
+// let name="ajay";
+// console.log(name.length);
 
-//use of templet litral
-let boy1="ajay";
-let boy2="anshu2";
+// //use of templet litral
+// let boy1="ajay";
+// let boy2="anshu2";
 
-let c = `${boy2} is a friend of ${boy1}`
-console.log(c);
+// let c = `${boy2} is a friend of ${boy1}`
+// console.log(c);
 
-//Escape Sequence character
-// let fruit = 'bana\'na';
-// console.log(fruit); //output = bana'na
+// //Escape Sequence character
+// // let fruit = 'bana\'na';
+// // console.log(fruit); //output = bana'na
 
-let fruit = 'bana\na';
-console.log(fruit); //output = banan (a will b3 printed in nxt line)
+// let fruit = 'bana\na';
+// console.log(fruit); //output = banan (a will b3 printed in nxt line)
 
 
-// /r
-let fruit2 = 'bana\rn';
-console.log(fruit2);
+// // /r
+// let fruit2 = 'bana\rn';
+// console.log(fruit2);
+
+
+//Methods in Strings(used to manipulate strings)
+
+// let nam = "ajay";
+// console.log(nam.toUpperCase());
+// console.log(nam.toLowerCase());
+
+// console.log(nam.slice(1,4));
+
+// console.log(nam.replace("a","B"));
+// let nam2= "     anshu";
+// console.log(nam.concat(" is a friend of ",nam2));
+
+// console.log(nam2.trim());
+
+//printing string using for loop
+// let a="aimmijmninb";
+// for(let i =0;i<a.length;i++){
+//     console.log(a);
+// }
+
+// let a ="ajay";
+// console.log(a.startsWith('a'));
+// console.log(a.endsWith('a'));
+
+
