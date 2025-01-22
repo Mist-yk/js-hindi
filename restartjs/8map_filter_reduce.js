ab597337b1fae4790c4fa28cfc96c9991746d869
Ajay Negi <ajnegi456@gmail.com>
2025-01-22 19:46:34 +0000

    map_filter_reduce.js

---
+++ restartjs/8map_filter_reduce.js
@@ -0,0 +1,60 @@
+//Map (map creates a new array for performing tasks on each element of the array) unlike for each)
+
+// let arr =[4,5,6,7];
+// let a =arr.map((value,index,array) =>{
+//   console.log(value,index,array);
+//   return value +1 ;
+// })
+// console.log(a);
+
+
+//Array filter method
+// let arr =[1,2,3,4,5];
+// let a2 = arr.filter((a)=>{
+//   return a<3
+// })
+// console.log(a2);
+
+
+//Array reduce method (return value)
+// let arr3 =[1,2,3,4,5,6,5,6];
+// let newarr = arr3.reduce((a1,a2)=>{
+//   return a1 + a2;
+// })
+// console.log(newarr);
+
+
+//create an array of numbers and take input from the user to add numbers to this array;
+// let arr = [1,2,3,4,5];
+// let a =prompt("enter numbers");
+// a = Number.parseInt(a);
+// arr.push(a);
+// console.log(arr);
+
+
+//keep adding numbers to the array in 1 until 0 is added to the array
+
+// let arr = [1,2,3,4,5];
+// while(a!=0){
+// let a =prompt("enter numbers");
+// a = Number.parseInt(a);
+// arr.push(a);}
+// console.log(arr);
+
+
+//filter for numbers divisible by 10 from a given array
+
+// let arr2 =[20,30,40,55,66];
+// let a2 =arr2.filter((a) =>{
+//   return a%10==0;
+// })
+// console.log(a2);
+
+
+//return square of values
+
+// let val =[1,2,3,4,5];
+// let a =val.map((value)=>{
+//   return (value *value);
+// })
+// console.log(a);
